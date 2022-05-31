6e976a7e9a61249c349ca6fd347e374f91a737ba
juhis92 <99727134+juhis92@users.noreply.github.com>
2022-05-31 23:33:03 +0530

    Checking in the Script to fetch the EC2 instances's metadata in JSON format

---
+++ Challenge2/ec2-metadata.py
@@ -0,0 +1,39 @@
+import json ;
+import requests ;
+
+# Base URL to get the keys
+baseUrl='http://169.254.169.254/latest/meta-data/' ;
+
+# This function get the values for the key and stores them in JSON format
+def createMetaDataJSONRecursively(subUrl) :
+  currentUrl = baseUrl + subUrl ;
+  jsonStructure = {};
+  metaData = requests.get(currentUrl) ;
+  keys= metaData.text.splitlines() ;
+  for k in keys :
+    if k[len(k)-1] == '/' :
+      jsonStructure[k[:-1]] = createMetaDataJSONRecursively(subUrl+ k);
+    else :
+      value =requests.get(currentUrl + k).text ;
+      try:
+        jsonStructure[k] =json.loads(value) ;
+      except ValueError:
+        jsonStructure[k] = value ;
+     
+  return jsonStructure ;
+
+# Get the key as an input from the user and print the corresponding value
+inputKey = input('enter key') ;
+inputKey=inputKey.strip();
+print('inputKey is = ',inputKey)
+result=createMetaDataJSONRecursively('') ;
+
+# if the key is null print the entire json
+if(inputKey != 'null'):
+  try :
+    print(result[inputKey]);
+  except KeyError :
+    print('key not found') ;
+else :
+  print(json.dumps(result,indent=4)) ;
+
