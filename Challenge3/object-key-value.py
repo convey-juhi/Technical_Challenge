36b6f476679d4437b523992030a4926dec7395e8
convey-juhi <106310000+convey-juhi@users.noreply.github.com>
2022-06-01 12:17:34 +0530

    Update object-key-value.py

--- Challenge3/object-key-value.py
+++ Challenge3/object-key-value.py
@@ -4,19 +4,22 @@ import json ;
 inputObject = input('Please enter the object in the form of key-value pair ')
 inputKey = input('Please enter the key to find its value ')
 
-# Converts the JSON object into Python dict
-currentObject = json.loads(inputObject)
+# Function which find thes value based on input object and key provided.
+def objectKeyValue(inputObject,inputKey):
+    # Converts the JSON object into Python dict
+    currentObject = json.loads(inputObject)
 
-# Splits the key based on the delimiter
-keySplit = inputKey.split("/")
+    # Splits the key based on the delimiter
+    keySplit = inputKey.split("/")
 
-# Traverses through the keys to get the value of the object
-for i in keySplit:
-    if (i in currentObject.keys()):
-        currentObject = currentObject[i];
-    else:
-        print('Invalid key entered')
-        quit()
+    # Traverses through the keys to get the value of the object
+    for i in keySplit:
+        if (i in currentObject.keys()):
+            currentObject = currentObject[i];
+        else:
+            print('Invalid key entered')
+            quit()
+    return(currentObject)
 
 # Print the value of the object
-print(currentObject)
+print(objectKeyValue(inputObject,inputKey))
